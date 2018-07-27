da30606a3239aa090312456a0c16ff005cecac63
Stephen Lang <saftslang@gmail.com>
2018-07-26 23:47:07 -0400

    Updating epel repo

--- scripts/centos7_rhel7_install_lamp.sh
+++ scripts/centos7_rhel7_install_lamp.sh
@@ -41,7 +41,7 @@
 yum -y update
 
 # Install EPEL and IUS Repo
-rpm -ivh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-10.noarch.rpm
+rpm -ivh http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-7-11.noarch.rpm
 rpm -ivh https://dl.iuscommunity.org/pub/ius/stable/CentOS/7/x86_64/ius-release-1.0-15.ius.centos7.noarch.rpm
 
 # Check to ensure EPEL repo was installed
